1cb0bab7a957f5d0d65e94d8276e2421ab7a3ab1
Wei-Cheng Wu <spencerwu85@gmail.com>
2018-09-28 13:38:07 +0800

    Update benchmarks
    Update benchmarks

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,17 +24,6 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  after  :finishing,    :restart
-end
-
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
