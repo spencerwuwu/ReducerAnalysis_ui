4a91fb14e3199ba46852d0c875e835331de6c5ec
Wei-Cheng Wu <spencerwu85@gmail.com>
2018-09-28 15:12:25 +0800

    Update conf

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '192.168.107.249', user: 'deploy', roles: %w{app db web}
+server '140.109.16.187', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
